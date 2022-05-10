eea8df0104d230d64f90fe03f5cf5a4170e055b6
oli-mac <adumulugeta21@gmail.com>
2022-05-10 14:03:41 +0300

    filter is done

--- index.js
+++ index.js
@@ -26,14 +26,44 @@
 const express = require('express');//import exprees
 
 const app = express();//assing value
+
+let users = [{
+    "id":"1",
+    "name":"abe"
+},{
+    "id":"2",
+    "name":"kebe"
+},{
+    "id":"3",
+    "name":"gube"
+}];
 //resolv end points
 app.get('/',(req,res)=>{
     res.send('hello bd');
 })//first parameter is always the url
 
-app.get('/courses',(req,res)=>{
-    res.send('hello bd courses');
+app.get('/name/:n',(req,res)=>{
+    res.send(req.params);
 })//first parameter is always the url
+// app.get('/name/:n',(req,res)=>{
+//     res.send('hello ${req.params}');
+// })//first parameter is always the url
+app.get('/namej/:n',(req,res)=>{
+    res.json('hello ${req.params}');
+})//display as a string like jason 
+app.get('/users',(req,res)=>{
+    res.send(users);
+})//display users lsit
+app.get('/users/:id',(req,res)=>{
+    const usr = users.find((u)=> u.id === req.params.id);
+
+    if(usr){
+        res.send(usr);
+        return;
+    }
+    res.status(404).send("user not found");
+
+})//display specifik user from users lsit
 //post end points
 app.listen(3000, ()=>{
     console.log("server connected");
